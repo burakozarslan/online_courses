e2c8ddb4b36ead7fb128072b1f6b9fdd5d295377
Burak Ozarslan <burakozarslan@icloud.com>
2025-12-26 16:06:28 +0300

    feat(player): track video playback progress
    - Added `useRef` hook for player instance
    - Implemented `logTimes` function
    - Log current time, duration, and progress
    - Integrated `onProgress` event handler
    - Prepare for video timestamp control

--- components/ui/VideoPlayer.tsx
+++ components/ui/VideoPlayer.tsx
@@ -15,8 +15,25 @@ import {
   MediaMuteButton,
   MediaFullscreenButton,
 } from "media-chrome/react";
+import { SyntheticEvent, useRef } from "react";
 
 export default function VideoPlayer() {
+  const playerRef = useRef(null);
+
+  const logTimes = (e: SyntheticEvent<HTMLVideoElement, Event>) => {
+    const currentTime = e.currentTarget.currentTime;
+    const duration = e.currentTarget.duration;
+    const timeLeft = duration - e.currentTarget.currentTime;
+    const progress = (currentTime / duration) * 100;
+
+    // TODO: Use this to start video at curtain timestamp.
+    e.currentTarget.currentTime = 5;
+
+    console.log("Current time: ", currentTime);
+    console.log("Time left: ", timeLeft);
+    console.log("Progress: ", progress);
+  };
+
   return (
     <MediaController
       style={{
@@ -25,9 +42,11 @@ export default function VideoPlayer() {
       }}
     >
       <ReactPlayer
+        ref={playerRef}
         slot="media"
         src="https://youtu.be/Hwsj5iS-rA0"
         controls={false}
+        onProgress={(e) => logTimes(e)}
         style={{
           width: "100%",
           height: "100%",
