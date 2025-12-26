6407e352854c201f95cf1d5fa025a654356c7155
Burak Ozarslan <burakozarslan@icloud.com>
2025-12-26 17:08:03 +0300

    feat(courses): add initial course data
    - Defines structure for courses
    - Includes modules and lessons
    - Adds sample MERN stack course

---
+++ courses.ts
@@ -0,0 +1,59 @@
+export const courses = [
+  {
+    title: "MERN Stack Tutorial",
+    description:
+      "Master the art of building headless e-commerce. Learn to connect Next.js App Router with Shopifys Storefront API and handle webhooks securely with Prisma and NextAuth.",
+    modules: [
+      {
+        number: "01",
+        title: "Introduction",
+        progress: 50,
+        isCompleted: false,
+        isActive: true,
+        lessons: [
+          {
+            title: "What is the MERN Stack?",
+            description:
+              "Master the art of building headless e-commerce. Learn to connect Next.js App Router with Shopifys Storefront API and handle webhooks securely with Prisma and NextAuth.",
+            duration: "7m 5s",
+            progress: 40,
+            isCurrent: true,
+          },
+          {
+            title: "Express App Setup",
+            description:
+              "Master the art of building headless e-commerce. Learn to connect Next.js App Router with Shopifys Storefront API and handle webhooks securely with Prisma and NextAuth.",
+            duration: "15m 31s",
+            progress: 0,
+            isCurrent: false,
+          },
+        ],
+      },
+      {
+        number: "02",
+        title: "MongoDB",
+        progress: 0,
+        isCompleted: false,
+        isActive: false,
+        lessons: [
+          {
+            title: "Express Router & API Routes",
+            description:
+              "Master the art of building headless e-commerce. Learn to connect Next.js App Router with Shopifys Storefront API and handle webhooks securely with Prisma and NextAuth.",
+            duration: "13m 38s",
+            progress: 40,
+            isCurrent: false,
+          },
+          {
+            title: "MongoDB Atlas & Mongoose",
+            description:
+              "Master the art of building headless e-commerce. Learn to connect Next.js App Router with Shopifys Storefront API and handle webhooks securely with Prisma and NextAuth.",
+            duration: "8m 49s",
+            progress: 0,
+            isCurrent: false,
+          },
+        ],
+      },
+    ],
+  },
+];
