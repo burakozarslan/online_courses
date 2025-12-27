bba93863e9c148382f22ca9e5036e20f86aa8f85
Burak Ozarslan <burakozarslan@icloud.com>
2025-12-27 18:28:35 +0300

    feat(video-player): add dynamic lesson playback
    - Accept `activeLesson` prop
    - Use `activeLesson.src` for video source
    - Resume playback from `activeLesson.time

--- components/ui/VideoPlayer.tsx
+++ components/ui/VideoPlayer.tsx
@@ -16,18 +16,23 @@ import {
   MediaFullscreenButton,
 } from "media-chrome/react";
 import { SyntheticEvent, useRef } from "react";
+import type { Lesson } from "@/courses";
 
-export default function VideoPlayer() {
+interface VideoPlayerProps {
+  activeLesson: Lesson;
+}
+
+export default function VideoPlayer({ activeLesson }: VideoPlayerProps) {
   const playerRef = useRef(null);
 
-  const logTimes = (e: SyntheticEvent<HTMLVideoElement, Event>) => {
+  const onProgress = (e: SyntheticEvent<HTMLVideoElement, Event>) => {
     const currentTime = e.currentTarget.currentTime;
     const duration = e.currentTarget.duration;
     const timeLeft = duration - e.currentTarget.currentTime;
     const progress = (currentTime / duration) * 100;
 
-    // TODO: Use this to start video at curtain timestamp.
-    e.currentTarget.currentTime = 5;
+    // TODO: Set the video time to where left off.
+    e.currentTarget.currentTime = activeLesson.timePlayed;
 
     console.log("Current time: ", currentTime);
     console.log("Time left: ", timeLeft);
@@ -44,9 +49,9 @@ export default function VideoPlayer() {
       <ReactPlayer
         ref={playerRef}
         slot="media"
-        src="https://youtu.be/Hwsj5iS-rA0"
+        src={activeLesson.src}
         controls={false}
-        onProgress={(e) => logTimes(e)}
+        onProgress={(e) => onProgress(e)}
         style={{
           width: "100%",
           height: "100%",
