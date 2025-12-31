2236a8ac4dad1e57831aee86f54026fbb6ea5a4c
Burak Ozarslan <burakozarslan@icloud.com>
2025-12-31 20:43:23 +0300

    feat(course): add authenticated course retrieval by slug - Add `getCourseBySlug` function - Retrieve course data using slug - Secure access with `withAuth` wrapper

--- actions.ts
+++ actions.ts
@@ -1,6 +1,17 @@
 import { db } from "./lib/prisma";
+import { Course } from "./app/generated/prisma/client";
+import { withAuth } from "./lib/auth-wrapper";
 
 export async function getAllCourses() {
   const courses = await db.course.findMany();
   return courses;
 }
+
+export const getCourseBySlug = withAuth(async (slug: Course["slug"]) => {
+  const course = await db.course.findUnique({
+    where: {
+      slug,
+    },
+  });
+  return course;
+});
